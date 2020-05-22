ad3891acf37c1b655e1f14c2428349a99c24eabe
Mark King <markpkng@gmail.com>
2020-05-22 01:03:19 -0400

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,44 +5,46 @@
  */
 
 module.exports = {
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-styled-components",
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
         },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                rule: {
-                    include: /assets/,
-                },
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `data`,
-                path: `${__dirname}/src/data/`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/assets/images`,
-                name: `images`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-s3`,
-            options: {
-              bucketName: "mark.codes",
-            },
-        },
-    ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/assets/images`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "mark.codes",
+        protocol: "https",
+        hostname: "www.mark.codes",
+      },
+    },
+  ],
 };
